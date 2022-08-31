e9bd03a3bf0c6eaa2cae62d97883b348899020f4
Mgoumane <goumane.mahmoudou@gmail.com>
2022-08-31 05:29:57 +0200

    etapes términées

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const Person = require('./models/Person');
 const Story = require('./models/Story');
+const studentModel = require("./models/Students");
+const addressModel = require("./models/Addresses");
 mongoose.connect(
     "mongodb://localhost:27017/populate",
     {useNewUrlParser: true, useUnifiedTopology: true},
@@ -8,7 +10,6 @@ mongoose.connect(
         console.log('DB connectée');
     }
 )
-
 // Ma première jointure
 Story.
   findOne({ title: 'Casino Royale' }).
@@ -17,4 +18,16 @@ Story.
     if (err) return handleError(err);
     console.log('The author is %s', story.author.name);
     // prints "The author is Ian Fleming"
+});
+
+// Serveur Back-end:
+const express = require("express");
+const port = 8000;
+const app = express();
+app.listen(port, () => {
+  console.log("Serveur lancé");
+});
+app.get("/students", async (req, res) => {
+  const students = await studentModel.find().populate("address").lean().exec();
+  res.send(students);
 });
